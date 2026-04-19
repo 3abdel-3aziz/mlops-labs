25606b41ba52788ccc77a30165c356433539ac8a
Abdelaziz <ahmed2025.mohamed2000@gmail.com>
2026-04-19 17:23:22 +0200

    stractured_files

---
+++ titanic-mlops-pipeline/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End-to-end MLOps pipeline for binary classification using the Titanic dataset, including data preprocessing, feature engineering, model training, and evaluation.',
+    author='3abdel-3aziz',
+    license='MIT',
+)
